290ce1153e93e024d009cebac8efccaf44d9e815
hasinduudara <hasindutechschool@gmail.com>
2025-05-08 19:40:45 +0530

    Add multiple click event handlers for button to trigger different alerts

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById("btn").addEventListener("click", function () {
-  alert("Hello World!");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  alert("Button clicked 1");
+});
+
+btn.addEventListener("click", function () {
+  alert("Button clicked 2");
 });
