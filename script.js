91610f14b7f07d1bb792337fa6ad1f3a41d6059b
hasinduudara <hasindutechschool@gmail.com>
2025-05-08 12:06:21 +0530

    Create html & js file

---
+++ script.js
@@ -0,0 +1,12 @@
+// normal function
+function greet1(name) {
+  return "Hello " + name + " !";
+}
+console.log(greet1("Hasindu"));
+
+// arrow function
+let greet2 = (name) => {
+  return "Hello " + name + " !";
+};
+
+console.log(greet2("Udara"));
