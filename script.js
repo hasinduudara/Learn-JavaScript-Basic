923c7336fc9a1bc793d75b69752cd453a81b3dc2
hasinduudara <hasindutechschool@gmail.com>
2025-05-08 19:54:15 +0530

    Replace label with input field and update event handler to log key presses

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ btn.addEventListener("click", function () {
   alert("Button clicked 1");
 });
 
-btn.addEventListener("click", function () {
-  alert("Button clicked 2");
+document.getElementById("myInput").addEventListener("keypress", function (e) {
+  console.log("Key pressed: " + e.key);
 });
